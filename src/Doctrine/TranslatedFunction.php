ffc774951083f0ea2955dc0f06c4af41fb290bbc
Matthieu Napoli <matthieu@mnapoli.fr>
2014-02-26 13:40:01 +0100

    Added the Doctrine TR function

---
+++ src/Doctrine/TranslatedFunction.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Mnapoli\Translated\Doctrine;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\AST\PathExpression;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * Walks the DQL AST.
+ *
+ * @author Matthieu Napoli <matthieu@mnapoli.fr>
+ */
+class TranslatedFunction extends FunctionNode
+{
+    /**
+     * @var PathExpression
+     */
+    private $field;
+
+    /**
+     * @param Parser $parser
+     *
+     * @return void
+     */
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $parser->match(Lexer::T_IDENTIFIER);
+        $identificationVariable = $parser->getLexer()->token['value'];
+
+        $parser->match(Lexer::T_DOT);
+
+        $parser->match(Lexer::T_IDENTIFIER);
+        $field = $parser->getLexer()->token['value'];
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+
+        $this->field = new PathExpression(PathExpression::TYPE_STATE_FIELD, $identificationVariable, $field);
+        $this->field->type = PathExpression::TYPE_STATE_FIELD;
+
+        // TODO how to pass the locale up to here?
+        $this->field->field .= '.en';
+    }
+
+    /**
+     * @param SqlWalker $sqlWalker
+     *
+     * @return string
+     */
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return $this->field->dispatch($sqlWalker);
+    }
+}
--- tests/DoctrineTest.php
+++ tests/DoctrineTest.php
@@ -3,6 +3,7 @@
 namespace Test\Mnapoli\Translated;
 
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query;
 use Doctrine\ORM\Tools\SchemaTool;
 use Doctrine\ORM\Tools\Setup;
 use Test\Mnapoli\Translated\Fixture\MyEntity;
@@ -27,6 +28,7 @@ class DoctrineTest extends \PHPUnit_Framework_TestCase
 
         // Create the entity manager
         $config = Setup::createAnnotationMetadataConfiguration($paths, true);
+        $config->addCustomStringFunction('TR', 'Mnapoli\Translated\Doctrine\TranslatedFunction');
         $this->em = EntityManager::create($dbParams, $config);
 
         // Create the DB
@@ -48,5 +50,27 @@ class DoctrineTest extends \PHPUnit_Framework_TestCase
         $entity2 = $this->em->find(get_class($entity1), $entity1->getId());
 
         $this->assertEquals($entity1->getName(), $entity2->getName());
+    }
+
+    public function testWhere()
+    {
+        $entity1 = new MyEntity();
+        $entity1->getName()->set('hello', 'en');
+        $entity1->getName()->set('bonjour', 'fr');
+        $this->em->persist($entity1);
+        $this->em->flush();
+
+        $results = $this->em->createQuery(
+            'SELECT e FROM Test\Mnapoli\Translated\Fixture\MyEntity e WHERE TR(e.name) = \'hello\''
+        )->getResult();
+
+        $this->assertCount(1, $results);
+        $this->assertSame($entity1, current($results));
+
+        $results = $this->em->createQuery(
+            'SELECT e FROM Test\Mnapoli\Translated\Fixture\MyEntity e WHERE TR(e.name) = \'bonjour\''
+        )->getResult();
+
+        $this->assertEmpty($results);
     }
 }
