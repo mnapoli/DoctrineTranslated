885b34e89d269f78d9421c46cf784bd21527dcfb
Matthieu Napoli <matthieu@mnapoli.fr>
2014-03-27 17:06:30 +0100

    TranslationUtils::getRegion()

--- src/TranslationUtils.php
+++ src/TranslationUtils.php
@@ -24,4 +24,22 @@ class TranslationUtils
 
         return substr($locale, 0, strpos($locale, '_'));
     }
+
+    /**
+     * Returns the region of the given locale.
+     *
+     * @param string $locale Locale (e.g. "en_US")
+     *
+     * @return string Language (e.g. "US")
+     */
+    public static function getRegion($locale)
+    {
+        if (strpos($locale, '_') === false) {
+            return null;
+        }
+
+        $region = substr($locale, strpos($locale, '_') + 1);
+
+        return ($region !== false) ? $region : null;
+    }
 }
--- tests/TranslationUtilsTest.php
+++ tests/TranslationUtilsTest.php
@@ -18,4 +18,14 @@ class TranslationUtilsTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('', TranslationUtils::getLanguage(''));
         $this->assertSame(null, TranslationUtils::getLanguage(null));
     }
+
+    public function testGetRegion()
+    {
+        $this->assertEquals('US', TranslationUtils::getRegion('en_US'));
+        $this->assertEquals('', TranslationUtils::getRegion('en'));
+        $this->assertSame('US_US', TranslationUtils::getRegion('en_US_US'));
+        $this->assertSame(null, TranslationUtils::getRegion('en_'));
+        $this->assertEquals(null, TranslationUtils::getRegion(''));
+        $this->assertSame(null, TranslationUtils::getRegion(null));
+    }
 }
